f354c2513abcad1eb38f9b676a65a2e11db4f2ed
Nolan <can.rochon@gmail.com>
2019-07-15 01:58:31 -0400

    Broke UI components into switch statement for initialization

--- src/MentalTracker/Pages/UserPromptPage.java
+++ src/MentalTracker/Pages/UserPromptPage.java
@@ -10,6 +10,7 @@ import com.codename1.ui.Label;
 import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.events.DataChangedListener;
+import com.codename1.ui.layouts.LayeredLayout;
 import com.codename1.ui.util.Resources;
 import static com.codename1.ui.util.Resources.getGlobalResources;
 
@@ -107,7 +108,7 @@ public class UserPromptPage extends Form{
     private void initGuiBuilderComponents(Resources resourceObjectInstance) {
         
         guiBuilderBindComponentListeners();
-        setLayout(new com.codename1.ui.layouts.LayeredLayout());
+        setLayout(new LayeredLayout());
         setInlineStylesTheme(resourceObjectInstance);
         setScrollableY(false);
         setInlineStylesTheme(resourceObjectInstance);
@@ -116,103 +117,137 @@ public class UserPromptPage extends Form{
         setName("UserPromptPage");
         addComponent(_Components.TopInset);
 
-        if (_Components.CheckInputButtons())
-        {
-            _Components.InputButtonNo.addActionListener(callback);
-            _Components.InputButtonYes.addActionListener(callback);
-        }
-        if (_Components.CheckSlider())
-        {
-            _Components.InputSlider.addActionListener(callback);
-            _Components.InputSlider.addDataChangedListener(callback);
-        }
-
         for (ComponantName component: _Components.EnabledComponents)
         {
             switch (component) {
-                case TOP_INSET: addComponent(_Components.TopInset); break;
-                case ADV_BUTTON: addComponent(_Components.AdvanceButton); break;
-                case BACK_BUTTON: addComponent(_Components.BackButton); break;
-                case INPUT_SLIDER: addComponent(_Components.InputSlider); break;
-                case PROMPT_TITLE: addComponent(_Components.PromptTitle); break;
-                case INPUT_BUTTON_NO: addComponent(_Components.InputButtonNo); break;
-                case INPUT_BUTTON_YES: addComponent(_Components.InputButtonYes); break;
-                case PROMPT_QUESTION: addComponent(_Components.PromptQuestion); break;
-                case INPUT_TEXT_FIELD: addComponent(_Components.InputTextField); break;
+                case TOP_INSET:
+                    addComponent(_Components.TopInset);
+                    _Components.TopInset.setPreferredSizeStr("inherit 4.2328043mm");
+                    _Components.TopInset.setInlineStylesTheme(resourceObjectInstance);
+                    _Components.TopInset.setName("topInset");
+                    ((LayeredLayout)_Components.TopInset.getParent().getLayout()).
+                            setInsets(_Components.TopInset, "1.6mm 5.0mm auto 5.0mm").
+                            setReferenceComponents(_Components.TopInset, "-1 -1 -1 -1").
+                            setReferencePositions(_Components.TopInset, "0.0 0.0 0.0 0.0");
+                    break;
+
+                case ADV_BUTTON:
+                    addComponent(_Components.AdvanceButton);
+                    _Components.AdvanceButton.setPreferredSizeStr("116.93122mm 23.544973mm");
+                    _Components.AdvanceButton.setInlineStylesTheme(resourceObjectInstance);
+                    _Components.AdvanceButton.setName("confirmButton");
+                    _Components.AdvanceButton.setIcon(resourceObjectInstance.getImage("continueButton.png"));
+                    _Components.AdvanceButton.setPressedIcon(resourceObjectInstance.getImage("continuePress.png"));
+                    ((LayeredLayout)_Components.AdvanceButton.getParent().getLayout()).
+                            setInsets(_Components.AdvanceButton, "auto 0.0mm 1.0582011mm 0.0mm").
+                            setReferenceComponents(_Components.AdvanceButton, "0 0 -1 0 ").
+                            setReferencePositions(_Components.AdvanceButton, "1.0 0.0 0.0 0.0");
+                    break;
+
+                case BACK_BUTTON:
+                    addComponent(_Components.BackButton);
+                    _Components.BackButton.setPreferredSizeStr("28.042328mm 10.8465605mm");
+                    _Components.BackButton.setInlineStylesTheme(resourceObjectInstance);
+                    _Components.BackButton.setName("backButton");
+                    _Components.BackButton.setIcon(resourceObjectInstance.getImage("backButton.png"));
+                    _Components.BackButton.setPressedIcon(resourceObjectInstance.getImage("backPressed.png"));
+                    ((LayeredLayout)_Components.BackButton.getParent().getLayout()).setInsets(_Components.BackButton, "0.0mm auto auto 0.0mm").setReferenceComponents(_Components.BackButton, "0 -1 -1 0 ").setReferencePositions(_Components.BackButton, "1.0 0.0 0.0 0.0");
+                    break;
+
+                case INPUT_SLIDER:
+                    addComponent(_Components.InputSlider);
+                    _Components.InputSlider.setPreferredSizeStr("88.88889mm 11.111112mm");
+                    _Components.InputSlider.setEditable(true);
+                    _Components.InputSlider.setFocusable(true);
+                    _Components.InputSlider.setScrollVisible(true);
+                    _Components.InputSlider.setText("Slider Here");
+                    _Components.InputSlider.setInlineStylesTheme(resourceObjectInstance);
+                    _Components.InputSlider.setInlineAllStyles("alignment:center;");
+                    _Components.InputSlider.setName("Slider");
+                    _Components.InputSlider.setGap(3);
+                    _Components.InputSlider.setIncrements(1);
+                    _Components.InputSlider.setProgress(5);
+                    ((LayeredLayout)_Components.InputSlider.getParent().getLayout()).
+                            setInsets(_Components.InputSlider, "0.0mm 0.0mm auto 0.0mm").
+                            setReferenceComponents(_Components.InputSlider, "2 2 8 2 ").
+                            setReferencePositions(_Components.InputSlider, "1.0 0.0 0.0 0.0");
+                    break;
+
+                case PROMPT_TITLE:
+                    addComponent(_Components.PromptTitle);
+
+                    break;
+
+                case INPUT_BUTTON_NO:
+                    addComponent(_Components.InputButtonNo);
+                    _Components.InputButtonYes.setPreferredSizeStr("18.78307mm 6.878307mm");
+                    _Components.InputButtonYes.setSelected(false);
+                    _Components.InputButtonYes.setUIID("ToggleButton");
+                    _Components.InputButtonYes.setInlineStylesTheme(resourceObjectInstance);
+                    _Components.InputButtonYes.setInlineAllStyles("border:none; bgColor:efefef;");
+                    _Components.InputButtonYes.setInlineSelectedStyles("border:none;");
+                    _Components.InputButtonYes.setInlinePressedStyles("border:none;");
+                    _Components.InputButtonYes.setGroup("depressedButtonGroup");
+                    _Components.InputButtonYes.setName("depressedButtonNo");
+                    _Components.InputButtonYes.setIcon(resourceObjectInstance.getImage("noButtonUnsele.png"));
+                    _Components.InputButtonYes.setPressedIcon(resourceObjectInstance.getImage("noButtonSelect.png"));
+                    ((LayeredLayout)_Components.InputButtonYes.getParent().getLayout()).
+                            setInsets(_Components.InputButtonYes, "0.0mm 0.0mm 0.0mm 0.0mm").
+                            setReferenceComponents(_Components.InputButtonYes, "6 17 6 10 ").
+                            setReferencePositions(_Components.InputButtonYes, "0.0 0.0 0.0 0.0");
+                    break;
+
+                case INPUT_BUTTON_YES:
+                    addComponent(_Components.InputButtonYes);
+                    _Components.InputButtonYes.setPreferredSizeStr("22.486773mm 6.6137567mm");
+                    _Components.InputButtonYes.setSelected(false);
+                    _Components.InputButtonYes.setUIID("ToggleButton");
+                    _Components.InputButtonYes.setInlineStylesTheme(resourceObjectInstance);
+                    _Components.InputButtonYes.setInlineAllStyles("border:none; bgColor:efefef;");
+                    _Components.InputButtonYes.setInlineSelectedStyles("border:none;");
+                    _Components.InputButtonYes.setInlinePressedStyles("border:none;");
+                    _Components.InputButtonYes.setGroup("depressedButtonGroup");
+                    _Components.InputButtonYes.setName("depressedButtonYes");
+                    _Components.InputButtonYes.setIcon(resourceObjectInstance.getImage("yesButtonUnselect.png"));
+                    _Components.InputButtonYes.setPressedIcon(resourceObjectInstance.getImage("yesButtonSelect.png"));
+                    ((LayeredLayout)_Components.InputButtonYes.getParent().getLayout()).
+                            setInsets(_Components.InputButtonYes, "7.632094% 0.0mm auto 0.0mm").
+                            setReferenceComponents(_Components.InputButtonYes, "2 16 -1 16 ").
+                            setReferencePositions(_Components.InputButtonYes, "0.0 0.0 0.0 0.0");
+                    break;
+
+                case PROMPT_QUESTION:
+                    addComponent(_Components.PromptQuestion);
+
+                    break;
+
+                case INPUT_TEXT_FIELD:
+                    addComponent(_Components.InputTextField);
+                    _Components.InputTextField.setHint("In Hours");
+                    _Components.InputTextField.setInlineStylesTheme(resourceObjectInstance);
+                    _Components.InputTextField.setName("sleepInput");
+                    _Components.InputTextField.setColumns(8);
+                    ((LayeredLayout)_Components.InputTextField.getParent().getLayout()).
+                            setInsets(_Components.InputTextField, "3.968254mm 37.200737% auto 30.939226%").
+                            setReferenceComponents(_Components.InputTextField, "5 0 -1 0 ").
+                            setReferencePositions(_Components.InputTextField, "1.0 0.0 0.0 0.0");
+                    break;
             }
         }
 
-        gui_topInset.setPreferredSizeStr("inherit 4.2328043mm");
-        gui_topInset.setInlineStylesTheme(resourceObjectInstance);
-        gui_topInset.setName("topInset");
-        ((com.codename1.ui.layouts.LayeredLayout)gui_topInset.getParent().getLayout()).setInsets(gui_topInset, "1.6mm 5.0mm auto 5.0mm").setReferenceComponents(gui_topInset, "-1 -1 -1 -1").setReferencePositions(gui_topInset, "0.0 0.0 0.0 0.0");
-        gui_moodSlider.setPreferredSizeStr("88.88889mm 11.111112mm");
-        gui_moodSlider.setEditable(true);
-        gui_moodSlider.setFocusable(true);
-        gui_moodSlider.setText("Mood Slider Here");
-        gui_moodSlider.setInlineStylesTheme(resourceObjectInstance);
-        gui_moodSlider.setInlineAllStyles("alignment:center;");
-        gui_moodSlider.setName("moodSlider");
-        gui_moodSlider.setIncrements(1);
-        gui_moodSlider.setProgress(5);
-        ((com.codename1.ui.layouts.LayeredLayout)gui_moodSlider.getParent().getLayout()).setInsets(gui_moodSlider, "0.0mm 0.0mm auto 0.0mm").setReferenceComponents(gui_moodSlider, "2 2 8 2 ").setReferencePositions(gui_moodSlider, "1.0 0.0 0.0 0.0");
-        gui_confidenceSlider.setPreferredSizeStr("85.978836mm 11.375662mm");
-        gui_confidenceSlider.setEditable(true);
-        gui_confidenceSlider.setFocusable(true);
-        gui_confidenceSlider.setText("Confidence Slider Here");
-        gui_confidenceSlider.setInlineStylesTheme(resourceObjectInstance);
-        gui_confidenceSlider.setInlineAllStyles("alignment:center;");
-        gui_confidenceSlider.setName("confidenceSlider");
-        gui_confidenceSlider.setIncrements(1);
-        gui_confidenceSlider.setProgress(3);
-        ((com.codename1.ui.layouts.LayeredLayout)gui_confidenceSlider.getParent().getLayout()).setInsets(gui_confidenceSlider, "auto 0.0mm 34.15493% 0.0mm").setReferenceComponents(gui_confidenceSlider, "-1 3 -1 3 ").setReferencePositions(gui_confidenceSlider, "0.0 0.0 0.0 0.0");
-        gui_anxietySlider.setPreferredSizeStr("89.94709mm 10.58201mm");
-        gui_anxietySlider.setEditable(true);
-        gui_anxietySlider.setFocusable(true);
-        gui_anxietySlider.setInfinite(false);
-        gui_anxietySlider.setScrollVisible(true);
-        gui_anxietySlider.setText("Anxiety Slider Here");
-        gui_anxietySlider.setUIID("SliderFull");
-        gui_anxietySlider.setInlineStylesTheme(resourceObjectInstance);
-        gui_anxietySlider.setInlineAllStyles("alignment:center;");
-        gui_anxietySlider.setName("anxietySlider");
-        gui_anxietySlider.setGap(2);
-        gui_anxietySlider.setIncrements(1);
-        gui_anxietySlider.setProgress(2);
-        ((com.codename1.ui.layouts.LayeredLayout)gui_anxietySlider.getParent().getLayout()).setInsets(gui_anxietySlider, "1.5873067mm 0.0mm 86.39456% 0.0mm").setReferenceComponents(gui_anxietySlider, "4 4 -1 4 ").setReferencePositions(gui_anxietySlider, "1.0 0.0 0.0 0.0");
-        gui_stressSlider.setPreferredSizeStr("89.68254mm 11.111112mm");
-        gui_stressSlider.setEditable(true);
-        gui_stressSlider.setFocusable(true);
-        gui_stressSlider.setText("Stress Slider Here");
-        gui_stressSlider.setInlineStylesTheme(resourceObjectInstance);
-        gui_stressSlider.setInlineAllStyles("alignment:center;");
-        gui_stressSlider.setName("stressSlider");
-        gui_stressSlider.setIncrements(1);
-        gui_stressSlider.setProgress(2);
-        ((com.codename1.ui.layouts.LayeredLayout)gui_stressSlider.getParent().getLayout()).setInsets(gui_stressSlider, "15.748086% 14.214464% auto 0.0mm").setReferenceComponents(gui_stressSlider, "7 0 -1 8 ").setReferencePositions(gui_stressSlider, "1.0 0.0 0.0 1.0");
-        gui_confirmButton.setPreferredSizeStr("116.93122mm 23.544973mm");
-        gui_confirmButton.setInlineStylesTheme(resourceObjectInstance);
-        gui_confirmButton.setName("confirmButton");
-        gui_confirmButton.setIcon(resourceObjectInstance.getImage("continueButton.png"));
-        gui_confirmButton.setPressedIcon(resourceObjectInstance.getImage("continuePress.png"));
-        ((com.codename1.ui.layouts.LayeredLayout)gui_confirmButton.getParent().getLayout()).setInsets(gui_confirmButton, "auto 0.0mm 1.0582011mm 0.0mm").setReferenceComponents(gui_confirmButton, "0 0 -1 0 ").setReferencePositions(gui_confirmButton, "1.0 0.0 0.0 0.0");
-        gui_backButton.setPreferredSizeStr("28.042328mm 10.8465605mm");
-        gui_backButton.setInlineStylesTheme(resourceObjectInstance);
-        gui_backButton.setName("backButton");
-        gui_backButton.setIcon(resourceObjectInstance.getImage("backButton.png"));
-        gui_backButton.setPressedIcon(resourceObjectInstance.getImage("backPressed.png"));
-        ((com.codename1.ui.layouts.LayeredLayout)gui_backButton.getParent().getLayout()).setInsets(gui_backButton, "0.0mm auto auto 0.0mm").setReferenceComponents(gui_backButton, "0 -1 -1 0 ").setReferencePositions(gui_backButton, "1.0 0.0 0.0 0.0");
+        
+        ((LayeredLayout)_Components.BackButton.getParent().getLayout()).setInsets(_Components.BackButton, "0.0mm auto auto 0.0mm").setReferenceComponents(_Components.BackButton, "0 -1 -1 0 ").setReferencePositions(_Components.BackButton, "1.0 0.0 0.0 0.0");
         gui_Label.setPreferredSizeStr("126.455025mm 24.338625mm");
         gui_Label.setInlineStylesTheme(resourceObjectInstance);
         gui_Label.setInlineAllStyles("alignment:center;");
         gui_Label.setName("Label");
         gui_Label.setIcon(resourceObjectInstance.getImage("moodLabel.png"));
-        ((com.codename1.ui.layouts.LayeredLayout)gui_Label.getParent().getLayout()).setInsets(gui_Label, "8.0% 0.0mm auto 0.0mm").setReferenceComponents(gui_Label, "0 0 -1 0 ").setReferencePositions(gui_Label, "1.0 0.0 0.0 0.0");
+        ((LayeredLayout)gui_Label.getParent().getLayout()).setInsets(gui_Label, "8.0% 0.0mm auto 0.0mm").setReferenceComponents(gui_Label, "0 0 -1 0 ").setReferencePositions(gui_Label, "1.0 0.0 0.0 0.0");
         gui_moodSlideLabels.setPreferredSizeStr("38.095238mm 47.089947mm");
         gui_moodSlideLabels.setInlineStylesTheme(resourceObjectInstance);
         gui_moodSlideLabels.setName("moodSlideLabels");
         gui_moodSlideLabels.setIcon(resourceObjectInstance.getImage("moodSlideLabels.png"));
-        ((com.codename1.ui.layouts.LayeredLayout)gui_moodSlideLabels.getParent().getLayout()).setInsets(gui_moodSlideLabels, "17.6318% -5.0264544mm 11.764706% -5.026455mm").setReferenceComponents(gui_moodSlideLabels, "7 6 5 0 ").setReferencePositions(gui_moodSlideLabels, "1.0 0.0 1.0 0.0");
+        ((LayeredLayout)gui_moodSlideLabels.getParent().getLayout()).setInsets(gui_moodSlideLabels, "17.6318% -5.0264544mm 11.764706% -5.026455mm").setReferenceComponents(gui_moodSlideLabels, "7 6 5 0 ").setReferencePositions(gui_moodSlideLabels, "1.0 0.0 1.0 0.0");
     }// </editor-fold>
 
 }
