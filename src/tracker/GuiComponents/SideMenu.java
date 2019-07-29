90aba2236313ed685d9c09459e851b1ac5e72f0c
Nolan <can.rochon@gmail.com>
2019-07-29 00:54:34 -0400

    Side Menu working; refactor main page

--- src/tracker/GuiComponents/SideMenu.java
+++ src/tracker/GuiComponents/SideMenu.java
@@ -1,8 +1,12 @@
 package tracker.GuiComponents;
 
 import com.codename1.ui.*;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.util.Resources;
+import tracker.Data.InstanceData;
+import tracker.Pages.ModularGUI.PromptList;
 
 /******************************************************************************
  * @author Nolan Rochon
@@ -18,9 +22,19 @@ public class SideMenu {
     /** If the sidemenu is added and initialized. */
     private boolean _IsInit;
 
-    public SideMenu(final Resources theme, final Toolbar tb) {
+    private InstanceData _Data;
+
+    private final String MAIN_PAGE = "Home";
+    private final String PROMTPS_LIST = "Questions Page";
+    private final String REMINDERS = "Reminders";
+    private final String ABOUT = "About";
+
+    public SideMenu(final Resources theme,
+                    final Toolbar tb,
+                    InstanceData data) {
         _Theme = theme;
         _PageTb = tb;
+        _Data = data;
         _IsInit = false;
 
         initialize();
@@ -30,20 +44,63 @@ public class SideMenu {
         Image sampleIcon = _Theme.getImage("icon.png");
         Container topBar = BorderLayout.east(new Label(sampleIcon));
 
+        SideMenuActionListener callback = new SideMenuActionListener();
+
         topBar.add(BorderLayout.SOUTH, new Label("Menu", "SideMenuTagline"));
         topBar.setUIID("SideCommand");
         _PageTb.addComponentToSideMenu(topBar);
 
         _PageTb.addMaterialCommandToSideMenu(
-                "Tracking", FontImage.MATERIAL_QUESTION_ANSWER, e -> { });
+                MAIN_PAGE, FontImage.MATERIAL_HOME, callback);
         _PageTb.addMaterialCommandToSideMenu(
-                "Settings", FontImage.MATERIAL_SETTINGS, e -> { });
+                PROMTPS_LIST, FontImage.MATERIAL_QUESTION_ANSWER, callback);
         _PageTb.addMaterialCommandToSideMenu(
-                "Profile", FontImage.MATERIAL_VERIFIED_USER, e -> { });
+                REMINDERS, FontImage.MATERIAL_NOTIFICATIONS, callback);
         _PageTb.addMaterialCommandToSideMenu(
-                "About", FontImage.MATERIAL_INFO, e -> { });
+                ABOUT, FontImage.MATERIAL_INFO, callback);
 
         _IsInit = true;
+    }
+
+    private void onMainPageSelected() {
+
+    }
+
+    private void onPromptsListSelected() {
+        PromptList promptList = new PromptList(_Data, _PageTb.getComponentForm());
+        promptList.show();
+    }
+
+    private void onRemindersSelected() {
+
+    }
+
+    private void onAboutSelected() {
+
+    }
+
+    class SideMenuActionListener implements ActionListener {
+
+        /**
+         * Invoked when an action occurred on a component.
+         *
+         * @param evt event object describing the source of the action as well
+         *            as its trigger
+         */
+        @Override
+        public void actionPerformed(ActionEvent evt) {
+            String sourceName = evt.getSource().toString();
+
+            if (sourceName.equals(MAIN_PAGE)) {
+                onMainPageSelected();
+            } else if (sourceName.equals(PROMTPS_LIST)) {
+                onPromptsListSelected();
+            } else if (sourceName.equals(REMINDERS)) {
+                onRemindersSelected();
+            } else if (sourceName.equals(ABOUT)) {
+                onAboutSelected();
+            }
+        }
     }
 
 }
