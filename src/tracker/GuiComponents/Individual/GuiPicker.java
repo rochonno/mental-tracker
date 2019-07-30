1b42a265b12a2ee06167868ce1c11cc702ded966
Nolan <can.rochon@gmail.com>
2019-07-30 12:05:30 -0400

    Redone prompt editor with TestPicker
    cant edit min and max with yes/no or string input

--- src/tracker/GuiComponents/Individual/GuiPicker.java
+++ src/tracker/GuiComponents/Individual/GuiPicker.java
@@ -1,5 +1,7 @@
 package tracker.GuiComponents.Individual;
 
+import com.codename1.ui.Component;
+import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.spinner.Picker;
 
 public class GuiPicker {
@@ -9,10 +11,26 @@ public class GuiPicker {
     GuiPicker(final String name, final int pickerType) {
         _Picker = new Picker();
         _Picker.setType(pickerType);
+        _Picker.setUseLightweightPopup(true);
         setName(name);
     }
 
-    Picker getPicker() {
+    public void centerAllign(boolean center) {
+        if (center) {
+            _Picker.setInlineAllStyles("alignment:center");
+            _Picker.getStyle().setAlignment(4);
+        }
+    }
+
+    public void centerVertically() {
+        _Picker.setVerticalAlignment(Component.CENTER);
+    }
+
+    public void setActionListener(ActionListener callback) {
+        _Picker.addActionListener(callback);
+    }
+
+    public Picker getPicker() {
         return _Picker;
     }
 
