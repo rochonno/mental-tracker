aaf1ec4727a2b1c7d68b21bc00973e0ba1b4156d
Nolan <can.rochon@gmail.com>
2019-07-29 21:49:46 -0400

    Clean up Main Page

--- src/tracker/Pages/ModularGUI/MainPage.java
+++ src/tracker/Pages/ModularGUI/MainPage.java
@@ -2,19 +2,17 @@ package tracker.Pages.ModularGUI;
 
 import com.codename1.ui.events.ActionListener;
 import tracker.Data.InstanceData;
+import tracker.GuiComponents.ImageNames;
+import tracker.GuiComponents.Individual.GuiButton;
+import tracker.GuiComponents.Individual.GuiImageViewer;
+import tracker.GuiComponents.Individual.GuiLabel;
 import tracker.GuiComponents.SideMenu;
 import tracker.Data.Prompts.*;
-import tracker.ComponentGenerators.MainPageComponents;
 import com.codename1.ui.Form;
 import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.layouts.LayeredLayout;
 import com.codename1.ui.plaf.UIManager;
 import com.codename1.ui.util.Resources;
-import tracker.Pages.LoginGUI;
-
-
-//STRICTLY FOR TESTING PURPOSES. DELETE LATER
-
 import static com.codename1.ui.util.Resources.getGlobalResources;
 
 /******************************************************************************
@@ -26,147 +24,145 @@ public class MainPage extends Form {
 
     private Form _Page;
     private SideMenu _SideMenu;
-    private MainPageComponents _Components;
+    private Resources _InstanceResources;
 
     private InstanceData _Data;
 
     public MainPage(InstanceData data) {
         _Data = data;
-        //runOtherPages();
+        _InstanceResources = getGlobalResources();
 
-        initialize();
-        _Components = new MainPageComponents();
-        initGuiBuilderComponents(getGlobalResources());
+        initializeData();
+        setSidePanel();
+        initializeLayout();
+        initializeComponents();
+        createComponents();
     }
 
-    private void runOtherPages() {
-
-        //_Page = new DailySurveyPgTwoGUI();
-        //_Page = new NeedDailyGUI();
-        //_Page = new DailySurveyPgOneGUI();
-        //_Page = new DailySurveyPgTwoGUI();
-        _Page = new LoginGUI();
-
-        /*MentalPrompt samplePrompt = new MentalPrompt("Exercise",
-                "Have you exercised in the past day?", PromptDataType.BOOL);
-        */
-
-        MentalPrompts samplePrompts =
-                GenerateDefaultPrompts.defaultMorningOnlyPrompts();
-        //_Page = new UserPromptPage(samplePrompts, results, null);
-
-        _Page = new CreatePromptPage(_Data, this);
-        //_Page = new CustomizeQuestions();
-
-        //_Page = new Form("MainPage");
-
-        //_Page.addComponent(new Label("Main Page"));
+    private void initializeLayout() {
+        setLayout(new LayeredLayout());
+        setInlineStylesTheme(_InstanceResources);
+        setScrollableY(false);
+        setInlineStylesTheme(_InstanceResources);
+        setInlineAllStyles("bgColor:efefef;");
+        setTitle("Mental Tracker");
+        setName("MainPage");
     }
 
-    private void onStartQuestionsEvent(final ActionEvent ev) {
+    private GuiLabel _TopInset;
+    private GuiLabel _PageTitle;
+    private GuiImageViewer _TrackerLogo;
+    private GuiButton _StartButton;
 
+    /**
+     * Initialize all the Gui components.
+     */
+    private void initializeComponents() {
+        _TopInset = new GuiLabel("TopInset", _InstanceResources);
+        _PageTitle = new GuiLabel("PageTitle", _InstanceResources);
+        _PageTitle.setText("Home");
+        _TrackerLogo = new GuiImageViewer("Logo", _InstanceResources, ImageNames.LOGO);
+
+        if (_Data.getHasAnswered()) {
+            return;
+        }
+        _StartButton = new GuiButton(
+                "StartButton",
+                _InstanceResources,
+                ImageNames.CONTINUE_UNSELECT,
+                ImageNames.CONTINUE_SELECT
+        );
+        _StartButton.setActionListener(new MainEventCallback());
+    }
+
+    /**
+     * Create all Gui components.
+     */
+    private void createComponents() {
+        createTopInset();
+        createPageTitle();
+        createTackerLogo();
+        createStartButton();
+    }
+
+    private void createTopInset() {
+        addComponent(_TopInset.getLabel());
+        _TopInset.setSizeStr("inherit 4.2328043mm");
+
+        ((LayeredLayout) _TopInset.getLabel().getParent().getLayout()).
+                setInsets(_TopInset.getLabel(), "1.6mm 5.0mm auto 5.0mm").
+                setReferenceComponents(_TopInset.getLabel(), "-1 -1 -1 -1").
+                setReferencePositions(_TopInset.getLabel(), "0.0 0.0 0.0 0.0");
+    }
+
+    private void createPageTitle() {
+        addComponent(_PageTitle.getLabel());
+        _PageTitle.getLabel().setAutoSizeMode(true);
+        if (!_Data.getHasAnswered()) {
+            _PageTitle.setText("How are you feeling today?");
+        } else {
+            _PageTitle.setText("Keep up the good work!");
+        }
+
+        ((LayeredLayout) _PageTitle.getLabel().getParent().getLayout()).
+                setInsets(_PageTitle.getLabel(), "0 auto auto auto").
+                setReferenceComponentTop(_PageTitle.getLabel(),
+                        _TopInset.getLabel(), 5f);
+    }
+
+    private void createTackerLogo() {
+        addComponent(_TrackerLogo.getImageViewer());
+
+        ((LayeredLayout) _TrackerLogo.getImageViewer().
+                getParent().getLayout()).
+                setInsets(_TrackerLogo.getImageViewer(), "0 auto auto auto").
+                setReferenceComponentTop(_TrackerLogo.getImageViewer(),
+                        _TopInset.getLabel(), 10f);
+    }
+
+    private void createStartButton() {
+        if (_StartButton == null) {
+            return;
+        }
+
+        _StartButton.setSizeStr("116.93122mm 23.544973mm");
+        addComponent(_StartButton.getButton());
+
+        ((LayeredLayout) _StartButton.getButton().
+                getParent().getLayout()).setInsets(
+                _StartButton.getButton(), "auto -125% 0 auto");
+    }
+
+    /**
+     * Called when the start button is pressed.
+     */
+    private void onStartQuestionsEvent() {
         // new questions page
         MultiPromptsPage prompt = new MultiPromptsPage(_Data, this, 0);
         prompt.show();
     }
 
-
-    private void guiBuilderBindComponentListeners() {
-        EventCallbackClass callback = new EventCallbackClass();
-
-        _Components.StartQuestions.addActionListener(callback);
-    }
-
-    class EventCallbackClass implements ActionListener {
-
-        EventCallbackClass() {
+    /**
+     * Event callback class for the start button on the main page.
+     */
+    class MainEventCallback implements ActionListener {
+        MainEventCallback() {
         }
 
         public void actionPerformed(final ActionEvent ev) {
-            com.codename1.ui.Component sourceComponent = ev.getComponent();
+            String sourceName = ev.getComponent().getName();
 
-            if (sourceComponent.getParent().getLeadParent() != null
-                    && (sourceComponent.getParent().getLeadParent()
-                    instanceof com.codename1.components.MultiButton
-                    || sourceComponent.getParent().getLeadParent()
-                    instanceof com.codename1.components.SpanButton)) {
-                sourceComponent = sourceComponent.getParent().getLeadParent();
-            }
-            if (sourceComponent == _Components.StartQuestions) {
-                onStartQuestionsEvent(ev);
+            if (sourceName.equals(_StartButton.getName())) {
+                onStartQuestionsEvent();
             }
 
         }
-    }
-
-    private void initGuiBuilderComponents(final Resources resourceInstance) {
-
-        guiBuilderBindComponentListeners();
-        setLayout(new LayeredLayout());
-        setInlineStylesTheme(resourceInstance);
-        setScrollableY(false);
-        setInlineStylesTheme(resourceInstance);
-        setInlineAllStyles("bgColor:efefef;");
-        setTitle("Mental Tracker");
-        setName("MainPage");
-
-        addComponent(_Components.TopInset);
-        _Components.TopInset.setPreferredSizeStr("inherit 4.2328043mm");
-        _Components.TopInset.setInlineStylesTheme(resourceInstance);
-        _Components.TopInset.setName("topInset");
-        ((LayeredLayout) _Components.TopInset.getParent().getLayout()).
-                setInsets(_Components.TopInset, "1.6mm 5.0mm auto 5.0mm").
-                setReferenceComponents(_Components.TopInset, "-1 -1 -1 -1").
-                setReferencePositions(_Components.TopInset, "0.0 0.0 0.0 0.0");
-
-        if (_Components.MentalTrackLogo != null) {
-            addComponent(_Components.MentalTrackLogo);
-            _Components.MentalTrackLogo.setImage(
-                    resourceInstance.getImage("Logo.png"));
-            _Components.MentalTrackLogo.setName("MentalTrackLogo");
-            ((LayeredLayout) _Components.MentalTrackLogo.
-                    getParent().getLayout()).
-                    setInsets(_Components.MentalTrackLogo, "0 auto auto auto").
-                    setReferenceComponentTop(_Components.MentalTrackLogo,
-                            _Components.TopInset, 10f);
-
-        }
-
-        if (!_Data.getHasAnswered()) {
-            addComponent(_Components.StartQuestions);
-            _Components.StartQuestions.setPreferredSizeStr(
-                    "116.93122mm 23.544973mm");
-            _Components.StartQuestions.setInlineStylesTheme(resourceInstance);
-            _Components.StartQuestions.setName("confirmButton");
-            _Components.StartQuestions.setIcon(
-                    resourceInstance.getImage("continueButton.png"));
-            _Components.StartQuestions.setPressedIcon(
-                    resourceInstance.getImage("continuePress.png"));
-            ((LayeredLayout) _Components.StartQuestions.
-                    getParent().getLayout()).
-                    setInsets(_Components.StartQuestions, "auto -125% 0 auto");
-        }
-
-
-        addComponent(_Components.PromptTitle);
-        _Components.PromptTitle.setName("Prompt");
-        _Components.PromptTitle.setAutoSizeMode(true);
-        if (!_Data.getHasAnswered()) {
-            _Components.PromptTitle.setText("Daily Tracking Is Ready!");
-        } else {
-            _Components.PromptTitle.setText("Keep Up the Good Work!");
-        }
-        ((LayeredLayout) _Components.PromptTitle.getParent().getLayout()).
-                setInsets(_Components.PromptTitle, "0 auto auto auto").
-                setReferenceComponentTop(_Components.PromptTitle,
-                        _Components.TopInset, 5f);
-
     }
 
     /**'
      * Components should be initialized here.
      */
-    private void initialize() {
+    private void initializeData() {
         /*
         try {
             _Prompts.loadPrompts(null);
@@ -180,10 +176,11 @@ public class MainPage extends Form {
         if (_Data.getInstanceResults() == null) {
             _Data.setInstanceResults(new PromptResults());
         }
-
-        setSidePanel();
     }
 
+    /**
+     * Creates the side panel for the Main Page.
+     */
     private void setSidePanel() {
         _SideMenu = new SideMenu(
                 UIManager.initFirstTheme("/theme"),
