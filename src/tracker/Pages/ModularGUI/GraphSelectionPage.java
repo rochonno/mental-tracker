569295091eb2aae448a4bfc0217f08b7eaa83b60
Nolan <can.rochon@gmail.com>
2019-08-05 15:42:17 -0400

    Create Demo Graphs - doug

--- src/tracker/Pages/ModularGUI/GraphSelectionPage.java
+++ src/tracker/Pages/ModularGUI/GraphSelectionPage.java
@@ -7,6 +7,8 @@ import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.layouts.LayeredLayout;
 import tracker.Data.InstanceData;
 import tracker.Data.Prompts.PromptDataType;
+import tracker.GuiComponents.GuiCharts.createDaySeriesForm;
+import tracker.GuiComponents.GuiCharts.createPieChartForm;
 import tracker.GuiComponents.Individual.GuiDatePicker;
 import tracker.GuiComponents.Individual.GuiLabel;
 import tracker.GuiComponents.Individual.GuiStringPicker;
@@ -238,6 +240,18 @@ public class GraphSelectionPage extends DefaultPageComponents {
     @Override
     void onConfirmButton() {
         if (checkInputComplete()) {
+            if (_GraphType == GraphTypes.PIE_CHART){
+                double[] tempVals = new double[]{10, 20};
+                String[] tempNames = new String[]{"Yes", "No"};
+                createPieChartForm temp = new createPieChartForm();
+                temp.createCategoricalPieChart(_PromptName, tempNames, tempVals);
+            }else if(_GraphType == GraphTypes.TREND_LINE){
+                double[] tempVals = new double[]{5, 7, 10, 12, 10, 3, 6, 5, 1, 2, 5, 10, 13, 4, 12,
+                9, 8, 7, 2, 5, 12, 10, 6, 7, 9, 8, 5, 5, 7, 4};
+                String[] tempNames = new String[]{_PromptName};
+                createDaySeriesForm temp = new createDaySeriesForm();
+                temp.getDaySeriesForm(_PromptName, "Day", "Unit");
+            }
 
         }
     }
