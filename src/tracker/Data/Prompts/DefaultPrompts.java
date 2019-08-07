2b238ef676176bdaa772ec6e78445dc5020d07b2
Nolan <can.rochon@gmail.com>
2019-08-07 06:06:27 -0400

    final javadoc test

--- src/tracker/Data/Prompts/DefaultPrompts.java
+++ src/tracker/Data/Prompts/DefaultPrompts.java
@@ -1,11 +1,10 @@
-/******************************************************************************
- * @author Nolan Rochon
- *
- * This class constructs the basic default Prompts for the user individually
- ******************************************************************************/
 package tracker.Data.Prompts;
 
-
+/******************************************************************************
+ * This class constructs the basic default Prompts for the user individually.
+ *
+ * @author Nolan Rochon
+ ******************************************************************************/
 final class DefaultPrompts {
 
     /** Default minimum value for slider input. */
